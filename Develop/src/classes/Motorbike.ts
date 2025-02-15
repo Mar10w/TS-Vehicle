2d654a528d302689621d013d51d544342d8fe989
D M <marlowthepirate@hotmail.com>
2025-02-15 13:28:01 -0600

    finished Motorbike.ts, altered wheelie function to POP

--- Develop/src/classes/Motorbike.ts
+++ Develop/src/classes/Motorbike.ts
@@ -4,18 +4,44 @@ import Wheel from './Wheel.js';
 
 // TODO: The Motorbike class should extend the Vehicle class
 class Motorbike {
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
   // TODO: Declare properties of the Motorbike class
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
-
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[]
+  ) {
+    super(vin, color, make, model, year, weight, topSpeed, wheels);
+  }
   // TODO: Create a constructor that accepts the properties of the Motorbike class
     // TODO: The constructor should call the constructor of the parent class, Vehicle
     // TODO: The constructor should initialize the properties of the Motorbike class
     // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
-
+popWheelie(): void {
+  console.log(`${this.make} ${this.model} is Popping a wheelie!`);
+} 
   // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
 
+printDetails(): void {
+  super.printDetails();
+  console.log(`Front Wheel: ${this.wheels[0].brand} ${this.wheels[0].diameter}"`);
+  console.log(`Rear Wheel: ${this.wheels[1].brand} ${this.wheels[1].diameter}"`);
+}
   // TODO: Override the printDetails method from the Vehicle class
   // TODO: The method should call the printDetails method of the parent class
   // TODO: The method should log the details of the Motorbike
